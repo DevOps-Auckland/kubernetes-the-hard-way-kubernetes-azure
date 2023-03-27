068768f4068435bedc4ebadbfd879d6c1737f02c
wilhelium <11941973+wilhelium@users.noreply.github.com>
2023-03-27 21:51:14 +1300

    Add Subnet and security group
    Why:
    
      * A subnet is required for a VM to be created in.
      * A Security group is required because Azure will deny everything by default.
    
    PBI: AB#14

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "azurerm" {
-  features { }
+  features {}
 }
 
 locals {
@@ -16,13 +16,42 @@ locals {
 }
 
 resource "azurerm_resource_group" "kubectlrg" {
-  name = "kubernetesrg"
+  name     = "kubernetesrg"
   location = local.location
 }
 
 resource "azurerm_virtual_network" "kubectlvnet" {
-  name = "kubernetesvnet"
-  location = local.location
+  name                = "kubernetesvnet"
+  location            = local.location
   resource_group_name = azurerm_resource_group.kubectlrg.name
-  address_space = [ "10.240.0.0/24" ]
+  address_space       = ["10.240.0.0/20"]
+}
+
+resource "azurerm_subnet" "kubesubnet" {
+  name                 = "kubernetes"
+  resource_group_name  = azurerm_resource_group.kubectlrg.name
+  virtual_network_name = azurerm_virtual_network.kubectlvnet.name
+  address_prefixes     = ["10.240.0.0/24"]
+}
+
+resource "azurerm_network_security_group" "allow_internal" {
+  name                = "kubernetes-the-hard-way-allow-internal"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.kubectlrg.name
+  security_rule = [ ]
+}
+
+resource "azurerm_network_security_rule" "inbound_tcp_rule" {
+    name                         = "icp_rule"
+    resource_group_name = azurerm_resource_group.kubectlrg.name
+    network_security_group_name = azurerm_network_security_group.allow_internal.name
+    description                  = "TCP is a protocol used in HTTP to make sure packets are delivered."
+    direction                    = "Inbound"
+    protocol                     = "Tcp"
+    source_port_range            = "*"
+    destination_port_ranges      = [22, 6443]
+    source_address_prefixes      = ["10.240.0.0/24", "10.200.0.0/16"]
+    destination_address_prefixes = ["10.240.0.0/24", "10.200.0.0/16"]
+    access                       = "Allow"
+    priority                     = 100
 }
