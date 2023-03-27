c206914258e120819af23d5e9e5717692f88b4b3
wilhelium <11941973+wilhelium@users.noreply.github.com>
2023-03-27 23:03:26 +1300

    Add Ingress Security Rule
    Why:
    
      * The Kubernetes instructions required these settings to allow inbound
      communication.

--- main.tf
+++ main.tf
@@ -38,20 +38,68 @@ resource "azurerm_network_security_group" "allow_internal" {
   name                = "kubernetes-the-hard-way-allow-internal"
   location            = local.location
   resource_group_name = azurerm_resource_group.kubectlrg.name
-  security_rule = [ ]
+  security_rule       = []
 }
 
-resource "azurerm_network_security_rule" "inbound_tcp_rule" {
-    name                         = "icp_rule"
-    resource_group_name = azurerm_resource_group.kubectlrg.name
-    network_security_group_name = azurerm_network_security_group.allow_internal.name
-    description                  = "TCP is a protocol used in HTTP to make sure packets are delivered."
-    direction                    = "Inbound"
-    protocol                     = "Tcp"
-    source_port_range            = "*"
-    destination_port_ranges      = [22, 6443]
-    source_address_prefixes      = ["10.240.0.0/24", "10.200.0.0/16"]
-    destination_address_prefixes = ["10.240.0.0/24", "10.200.0.0/16"]
-    access                       = "Allow"
-    priority                     = 100
+resource "azurerm_network_security_rule" "inbound_internal_rule" {
+  for_each = {
+    Tcp  = 100
+    Udp  = 101
+    Icmp = 102
+  }
+  name                         = join("", [each.key, "_rule"])
+  resource_group_name          = azurerm_resource_group.kubectlrg.name
+  network_security_group_name  = azurerm_network_security_group.allow_internal.name
+  direction                    = "Inbound"
+  protocol                     = each.key
+  source_port_range            = "*"
+  destination_port_range       = "*"
+  source_address_prefixes      = ["10.240.0.0/24", "10.200.0.0/16"]
+  destination_address_prefixes = ["10.240.0.0/24", "10.200.0.0/16"]
+  access                       = "Allow"
+  priority                     = each.value
+}
+
+resource "azurerm_network_security_group" "allow_external" {
+  name                = "kubernetes-the-hard-way-allow-internal"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.kubectlrg.name
+  security_rule       = []
+}
+resource "azurerm_network_security_rule" "inbound_external_rule_tcp" {
+  name                         = "external_tcp"
+  resource_group_name          = azurerm_resource_group.kubectlrg.name
+  network_security_group_name  = azurerm_network_security_group.allow_external.name
+  direction                    = "Inbound"
+  protocol                     = "Tcp"
+  source_port_range            = "*"
+  destination_port_ranges      = [22, 6443]
+  source_address_prefix        = "122.60.7.3/32"
+  destination_address_prefixes = ["10.240.0.0/24", "10.200.0.0/16"]
+  access                       = "Allow"
+  priority                     = 300
+}
+
+resource "azurerm_network_security_rule" "inbound_external_rule_icmp" {
+  name                         = "external_icmp"
+  resource_group_name          = azurerm_resource_group.kubectlrg.name
+  network_security_group_name  = azurerm_network_security_group.allow_external.name
+  direction                    = "Inbound"
+  protocol                     = "Tcp"
+  source_port_range            = "*"
+  destination_port_ranges      = [22, 6443]
+  source_address_prefix        = "122.60.7.3/32"
+  destination_address_prefixes = ["10.240.0.0/24", "10.200.0.0/16"]
+  access                       = "Allow"
+  priority                     = 301
+}
+
+resource "azurerm_subnet_network_security_group_association" "kubesubnet_internal_security_rules" {
+  subnet_id                 = azurerm_subnet.kubesubnet.id
+  network_security_group_id = azurerm_network_security_group.allow_internal.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "kubesubnet_external_security_rules" {
+  subnet_id                 = azurerm_subnet.kubesubnet.id
+  network_security_group_id = azurerm_network_security_group.allow_external.id
 }
