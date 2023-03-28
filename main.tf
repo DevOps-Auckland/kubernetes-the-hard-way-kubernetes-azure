08f0bfb0e5acc30c040201bc04a81bf9de390db3
wilhelium <11941973+wilhelium@users.noreply.github.com>
2023-03-28 21:11:07 +1300

    Add Security rules to NSG
    Why:
    
      * Terraform put output as rules being deleted even if nothing changed.
    
    PBI: None

--- main.tf
+++ main.tf
@@ -38,59 +38,124 @@ resource "azurerm_network_security_group" "kubernetes_subnet_security" {
   name                = "kubernetes-the-hard-way-allow-internal"
   location            = local.location
   resource_group_name = azurerm_resource_group.kubectlrg.name
-  security_rule       = []
-}
-
-resource "azurerm_network_security_rule" "inbound_internal_rule" {
-  for_each = {
-    Tcp  = 100
-    Udp  = 101
-    Icmp = 102
-  }
-  name                         = join("", [each.key, "_rule"])
-  resource_group_name          = azurerm_resource_group.kubectlrg.name
-  network_security_group_name  = azurerm_network_security_group.kubernetes_subnet_security.name
-  direction                    = "Inbound"
-  protocol                     = each.key
-  source_port_range            = "*"
-  destination_port_range       = "*"
-  source_address_prefixes      = ["10.240.0.0/24", "10.200.0.0/16"]
-  destination_address_prefixes = ["10.240.0.0/24", "10.200.0.0/16"]
-  access                       = "Allow"
-  priority                     = each.value
-}
-
-resource "azurerm_network_security_rule" "inbound_external_rule_tcp" {
-  name                         = "external_tcp"
-  resource_group_name          = azurerm_resource_group.kubectlrg.name
-  network_security_group_name  = azurerm_network_security_group.kubernetes_subnet_security.name
-  direction                    = "Inbound"
-  protocol                     = "Tcp"
-  source_port_range            = "*"
-  destination_port_ranges      = [22, 6443]
-  source_address_prefix        = "122.60.7.3/32"
-  destination_address_prefixes = ["10.240.0.0/24", "10.200.0.0/16"]
-  access                       = "Allow"
-  priority                     = 300
-}
-
-resource "azurerm_network_security_rule" "inbound_external_rule_icmp" {
-  name                         = "external_icmp"
-  resource_group_name          = azurerm_resource_group.kubectlrg.name
-  network_security_group_name  = azurerm_network_security_group.kubernetes_subnet_security.name
-  direction                    = "Inbound"
-  protocol                     = "Icmp"
-  source_port_range            = "*"
-  destination_port_range       = "*"
-  source_address_prefix        = "122.60.7.3/32"
-  destination_address_prefixes = ["10.240.0.0/24", "10.200.0.0/16"]
-  access                       = "Allow"
-  priority                     = 301
-}
-
-resource "azurerm_subnet_network_security_group_association" "kubesubnet_security_rules" {
-  subnet_id                 = azurerm_subnet.kubesubnet.id
-  network_security_group_id = azurerm_network_security_group.kubernetes_subnet_security.id
+  security_rule       = [
+   {
+       access                                     = "Allow"
+       description                                = ""
+       destination_address_prefix                 = ""
+       destination_address_prefixes               = [
+           "10.200.0.0/16",
+           "10.240.0.0/24",
+                ]
+       destination_application_security_group_ids = []
+       destination_port_range                     = ""
+       destination_port_ranges                    = [
+           "22",
+           "6443",
+                ]
+       direction                                  = "Inbound"
+       name                                       = "external_tcp"
+       priority                                   = 300
+       protocol                                   = "Tcp"
+       source_address_prefix                      = "122.60.7.3/32"
+       source_address_prefixes                    = []
+       source_application_security_group_ids      = []
+       source_port_range                          = "*"
+       source_port_ranges                         = []
+            },
+   {
+       access                                     = "Allow"
+       description                                = ""
+       destination_address_prefix                 = ""
+       destination_address_prefixes               = [
+           "10.200.0.0/16",
+           "10.240.0.0/24",
+                ]
+       destination_application_security_group_ids = []
+       destination_port_range                     = "*"
+       destination_port_ranges                    = []
+       direction                                  = "Inbound"
+       name                                       = "Icmp_rule"
+       priority                                   = 102
+       protocol                                   = "Icmp"
+       source_address_prefix                      = ""
+       source_address_prefixes                    = [
+           "10.200.0.0/16",
+           "10.240.0.0/24",
+                ]
+       source_application_security_group_ids      = []
+       source_port_range                          = "*"
+       source_port_ranges                         = []
+            },
+   {
+       access                                     = "Allow"
+       description                                = ""
+       destination_address_prefix                 = ""
+       destination_address_prefixes               = [
+           "10.200.0.0/16",
+           "10.240.0.0/24",
+                ]
+       destination_application_security_group_ids = []
+       destination_port_range                     = "*"
+       destination_port_ranges                    = []
+       direction                                  = "Inbound"
+       name                                       = "Tcp_rule"
+       priority                                   = 100
+       protocol                                   = "Tcp"
+       source_address_prefix                      = ""
+       source_address_prefixes                    = [
+           "10.200.0.0/16",
+           "10.240.0.0/24",
+                ]
+       source_application_security_group_ids      = []
+       source_port_range                          = "*"
+       source_port_ranges                         = []
+            },
+   {
+       access                                     = "Allow"
+       description                                = ""
+       destination_address_prefix                 = ""
+       destination_address_prefixes               = [
+           "10.200.0.0/16",
+           "10.240.0.0/24",
+                ]
+       destination_application_security_group_ids = []
+       destination_port_range                     = "*"
+       destination_port_ranges                    = []
+       direction                                  = "Inbound"
+       name                                       = "Udp_rule"
+       priority                                   = 101
+       protocol                                   = "Udp"
+       source_address_prefix                      = ""
+       source_address_prefixes                    = [
+           "10.200.0.0/16",
+           "10.240.0.0/24",
+                ]
+       source_application_security_group_ids      = []
+       source_port_range                          = "*"
+       source_port_ranges                         = []
+            },
+   {
+       access                                     = "Allow"
+       description                                = ""
+       destination_address_prefix                 = ""
+       destination_address_prefixes               = [
+           "10.200.0.0/16",
+           "10.240.0.0/24",
+                ]
+       destination_application_security_group_ids = []
+       destination_port_range                     = "*"
+       destination_port_ranges                    = []
+       direction                                  = "Inbound"
+       name                                       = "external_icmp"
+       priority                                   = 301
+       protocol                                   = "Icmp"
+       source_address_prefix                      = "122.60.7.3/32"
+       source_address_prefixes                    = []
+       source_application_security_group_ids      = []
+       source_port_range                          = "*"
+       source_port_ranges                         = []
+            },]
 }
 
 resource "azurerm_public_ip" "kubepublicip" {
@@ -101,5 +166,4 @@ resource "azurerm_public_ip" "kubepublicip" {
   lifecycle {
     create_before_destroy = true
   }
-}
 }
